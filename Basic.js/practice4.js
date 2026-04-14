4e9fbecf4f3c17aa91e03114ae9a17bd8f94b380
Usaid-Ansari <ansariusaid005@gmail.com>
2026-04-14 16:42:12 +0000

    basics of array

---
+++ Basic.js/practice4.js
@@ -0,0 +1,35 @@
+// let arr1 = new Array("purple", "green", "yellow");
+// let arr2 = ["black", "orange", "pink"];
+// console.log(arr1);
+// console.log(arr2);
+
+// let arr3 = new Array(10);
+// let arr4 = 10;
+// console.log(arr3);
+// console.log(arr4);
+
+
+// const arr = ["hi there"];
+// arr[0] = "new value";
+// console.log(arr[0]);
+
+//  colors = ["black", "orange", "pink"]
+// // booleans = [true, false, false, true];
+// // emptyArray = [];
+// console.log("Length of colors:", colors.length);
+// console.log("Length of booleans:", booleans.length);
+// console.log("Length of empty array:", emptyArray.length)
+
+// lastElement = colors[colors -1]; // colors is array not a number so js trie to do something like this ["black", "orange", "pink"] - 1 ;so that why they show undefiend
+// console.log(lastElement); 
+
+// let numbers = [12, 24, 36];
+// numbers[5] = 48;
+// console.log(numbers.length);
+
+// console.log("numbers", numbers);
+
+let fruits = ["Milk", "Bread"," Apple"];
+console.log("fruits", fruits.length);
+fruits[1] ="Banana";
+console.log(fruits);
