c316d4257f46931f47e66f39649dcdc9b73a24cc
Usaid-Ansari <ansariusaid005@gmail.com>
2026-04-14 16:41:45 +0000

    learning array method

---
+++ Basic.js/arraymethod.js
@@ -0,0 +1,17 @@
+//Array Method 
+let arr = ["grapefruit", "orange", "lemon"];
+arr.push("tangerine");
+console.log(arr);
+// arr.pop();
+// console.log (arr);
+// arr.shift();
+// console.log(arr);
+// arr.splice(2, 1, "square", "circle");
+// console.log(arr);
+// arr.splice(1, 3);
+// console.log(arr);
+
+let arr5 = [1, 2, 3];
+let arr6 = [4, 5, 6];
+let arr7 = arr5.concat(arr6);
+console.log(arr7);
