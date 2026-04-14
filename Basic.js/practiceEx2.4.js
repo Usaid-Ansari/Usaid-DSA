beab3ce00d327362ec689b918db189010a2362ae
Usaid-Ansari <ansariusaid005@gmail.com>
2026-04-14 09:15:02 +0000

    add some problem

---
+++ Basic.js/practiceEx2.4.js
@@ -0,0 +1,19 @@
+// Practice exercise 2.4
+// Create variables for three numbers: a, b, and c. Update these variables with the 
+// following actions using the assignment operators:
+// • Add b to a
+// • Divide a by c
+// [ 39 ]
+// JavaScript Essentials
+// • Replace the value of c with the modulus of c and b
+// • Print all three numbers to the console
+
+let a = 20;
+let b = 10;
+let c =10;
+console.log(b+a);
+console.log(a/c);
+c = c % b;
+console.log(a);
+console.log(b);
+console.log(c);
