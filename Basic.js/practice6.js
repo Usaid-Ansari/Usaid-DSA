8895cabf8cfb79489523ff2615ccfdefd228a01c
Usaid-Ansari <ansariusaid005@gmail.com>
2026-04-15 16:23:44 +0000

    added some array probelm

---
+++ Basic.js/practice6.js
@@ -0,0 +1,34 @@
+//Multidimensional Array
+
+// let someValues1 = [1, 2, 3];
+// let someValues2 = [4, 5, 6];
+// let someValues3 = [7, 8, 9];
+
+// let arrOfArrays = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+// console.log(arrOfArrays);
+
+// let value1 = arrOfArrays[0] [1];
+// let value2 = arrOfArrays[1] [1];
+// let value3 = arrOfArrays[2] [2];
+
+// let arrOfArrayofArrays = [arrOfArrays, arrOfArrays, arrOfArrays];
+// console.log (arrOfArrayofArrays);
+
+// let middleValue = arrOfArrayofArrays[2][2][2];  //[Layer][Row][column]
+// console.log(middleValue);
+
+// let arr = [
+//   [10, 20, 30],
+//   [40, 50, 60],
+//   [70, 80, 90]
+// ];
+// let findArrindex = arr[1][2];
+// console.log(findArrindex);
+
+let arr1 = [
+    [10, 20, 30],
+    [40, 50, 60],
+    [90, 80, 70]
+];
+let findIndex = arr1[1][1];
+console.log(findIndex);
