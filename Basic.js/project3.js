e0df2dce1aded7975075b59acfd813866c91d3ef
Usaid-Ansari <ansariusaid005@gmail.com>
2026-04-16 07:05:12 +0000

    i made a mobile company catlog to store a comapny data

---
+++ Basic.js/project3.js
@@ -0,0 +1,50 @@
+// const theList = ['Laurence', 'Svekis', true, 35]; 
+// theList.shift();
+// console.log(theList);
+// theList.pop();
+// console.log(theList);
+// theList.pop();
+// console.log(theList);
+// theList.unshift("FIRST");
+// console.log(theList);
+// theList[3] = "hello world";
+// theList[2] = "MIDDLE";
+// console.log(theList);
+// theList.push("LAST");
+// console.log(theList);
+
+// Company product catalog
+// In this project, you will implement a data structure for a product catalog and create 
+// queries to retrieve data.
+// 1. Create an array to hold an inventory of store items.
+// 2. Create three items, each having the properties of name, model, cost, and 
+// quantity.
+// 3. Add all three objects to the main array using an array method, and then log 
+// the inventory array to the console.
+// 4. Access the quantity element of your third item, and log it to the console. 
+// Experiment by adding and accessing more elements within your data 
+// structure.
+
+
+let obj1 = {
+    name : "Redmi",
+    model : "Note 9",
+    cost : "9000",
+    quantity : " 10",
+}
+let obj2 = {
+    name : "Poco",
+    model : "M6",
+    cost : "1200",
+    quantity : "50",
+}
+let obj3 = {
+    name : "One-Plus",
+    model : "Nord 7",
+    cost : "20000",
+    quantity : " 100",
+}
+
+let array = [obj1, obj2, obj3];
+console.log(array);
+console.log("The quantity is :",obj3.quantity);
