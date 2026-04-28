9a7fb2e5aa11dbcdd419376335a6851dabd0baea
Usaid-Ansari <ansariusaid005@gmail.com>
2026-04-28 16:46:18 +0000

    Medicine search system using JavaScript

--- number guessing game/app.js
+++ number guessing game/app.js
@@ -1,21 +1,27 @@
-let min = 1;
-let max = 5;
+let arr = [
 
-let number = Math.floor(Math.random() * max) + 1;
+    { name: "Arad Khurma", type: "Majun" },
+    { name: "Falasfa", type: "Majun" },
+    { name: "Dabeedul Ward", type: "Majun" },
+    { name: "Injeer", type: "Majun" },
+    { name: "Motadil", type: "sharbat" },
+    { name: "Dinar", type: "sharbat" },
+    { name: "Banfsha", type: "sharbat" },
+    { name: "Unnab", type: "sharbat" },
 
-let status = false;
-
-while (status == false) {
-    let userGuess = prompt("Enter a Number:");
-    userGuess = parseInt(userGuess);
-
-    if (userGuess === number) {
-        console.log("Correct!");
-        status = true;
-    } else if (userGuess > number) {
-        console.log("Too High");
-
-    } else {
-        console.log("Too Low");
+]
+let searchMedicine = prompt("Enter a medicine name:");
+let found = false;
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i].name.toLowerCase().trim() === searchMedicine.toLowerCase().trim()) {
+        found = true;
+        console.log(arr[i].name, "Available");
+        break;
     }
 }
+if (found === false) {
+    console.log("Not Available")
+}
+
+
+
